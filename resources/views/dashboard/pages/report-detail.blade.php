2d447fe3a7191f0c75bd4b5caddaf0d0498c065d
YeYintKo <yeyintko@ucs-myitkyina.edu.mm>
2022-04-12 12:11:54 +0630

    fixed detail page

--- resources/views/dashboard/pages/report-detail.blade.php
+++ resources/views/dashboard/pages/report-detail.blade.php
@@ -1,12 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
+    <div class="card ">
+        <div class="card-header d-flex justify-content-between">
             <h5>Detail</h5>
+          <div>
+
+            <button class="btn btn-primary rounded mr-2" type="button">Button</button>
+            <button class="btn btn-primary rounded mr-2" type="button">Button</button>
+            <button class="btn btn-primary rounded mr-2" type="button">Button</button>
+          </div>
         </div>
-       <div class="card-block">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi porro iusto deleniti enim reiciendis quisquam rem hic veniam corrupti fugit unde, expedita maxime ipsam asperiores, deserunt neque, accusantium possimus rerum?
-       </div>
+        <div class="d-flex px-2">
+        <div class="card ml-3 mt-5" style="flex: 0 0 300px">
+
+
+          <div class="card-body deta">
+            <img src="http://127.0.0.1:8000/jpg/avatar-5.jpg" height="150" class=" object-cover rounded mb-3 d-block " alt="">
+           <div class="card-text d-flex">   <p style="width: 100px">အမည် </p> <p class="mr-3"> : </p> <p>Mrs. Jailyn Gleichner </p> </div>
+           <div class="card-text d-flex">   <p style="width: 100px">အရပ်အမြင် </p> <p class="mr-3"> : </p> <p>3 </p> </div>
+           <div class="card-text d-flex">   <p style="width: 100px">ဖုန်းနံပတ် </p> <p class="mr-3"> : </p> <p>+1 (315) 577-7560</p> </div>
+           <div class="card-text d-flex">   <p style="width: 100px">အသက် </p> <p class="mr-3"> : </p> <p>6 </p> </div>
+
+
+
+
+        </div>
+      </div>
+      <div class="card ml-3 mr-3 mt-5" style="width: 100%">
+
+
+        <div class="card-body deta">
+            <div class="card-text d-flex">   <p style="flex: 0 0 120px">နေရပ်လိပ်စာ </p> <p class="mr-3"> : </p> <p>761 Sanford Plains Apt. 375 South Pansyburgh, GA 07311</p> </div>
+            <div class="card-text d-flex">   <p style="flex: 0 0 120px">အကြောင်းရာ </p> <p class="mr-3"> : </p> <p>Asperiores occaecati aut accusamus nemo molestiae. Tempore ipsum mollitia illo accusantium id vel eius. Libero dicta voluptates quibusdam aut.</p> </div>
+            <div class="card-text d-flex">   <p style="flex: 0 0 120px">physical Appearance </p> <p class="mr-3"> : </p> <p>Qui ut vero labore velit nobis facilis in aut. Commodi itaque eligendi nemo tempore et. Eum beatae quis eaque expedita eum. Ut quisquam vel perferendis accusantium accusamus est.</p> </div>
+            <div class="card-text d-flex">   <p style="flex: 0 0 120px">မှတ်ပုံတင်အမည် </p> <p class="mr-3"> : </p> <p>761 Sanford Plains Apt. 375 South Pansyburgh, GA 07311</p> </div>
+            <div class="card-text d-flex">   <p style="flex: 0 0 120px">အသက် </p> <p class="mr-3"> : </p> <p>761 Sanford Plains Apt. 375 South Pansyburgh, GA 07311</p> </div>
+
+
+
+
+
+      </div>
+    </div>
+          </div>
     </div>
 @endsection
